fa153d7e7bbcfc1fff5f11a18cf893a82e834ebf
Jose Ramirez <jramirez@Joses-MacBook-Pro.local>
2020-02-08 01:49:16 +0100

    delete vimrc add install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
-set runtimepath+=~/.vim_runtime
+#!/bin/sh
+set -e
+
+cd ~/.vim_runtime
+
+echo 'set runtimepath+=~/.vim_runtime
 
 source ~/.vim_runtime/vimrcs/base.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 " source ~/.vim_runtime/vimrcs/plugins_config.vim // no plugins yet :P
 source ~/.vim_runtime/vimrcs/extended.vim
+' > ~/.vimrc
+
+echo "🚀"
