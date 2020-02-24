462842d6494ea47b62b7b3e01e7969c6d768b634
Jose Ramirez <jramirez@Joses-MacBook-Pro.local>
2020-02-24 01:18:25 +0100

    add initial version of plugins file use minpac

--- install.sh
+++ install.sh
@@ -3,12 +3,19 @@ set -e
 
 cd ~/.vim_runtime
 
+echo '" Install minpac to manage plugins.'
+git clone https://github.com/k-takata/minpac.git ~/.vim_runtime/pack/minpac/opt/minpac
+
 echo 'set runtimepath+=~/.vim_runtime
 
 source ~/.vim_runtime/vimrcs/base.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
-" source ~/.vim_runtime/vimrcs/plugins_config.vim // no plugins yet :P
+source ~/.vim_runtime/vimrcs/plugins.vim
 source ~/.vim_runtime/vimrcs/extended.vim
 ' > ~/.vimrc
 
-echo "🚀"
+echo "
+
+🚀🚀🚀🚀🚀
+
+"
